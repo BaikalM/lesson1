3e76cfe2360f9c49d9c34fa1497442e40f575dd4
BaikalM <doktorpilman@gmail.com>
2021-09-15 13:23:11 +0300

    test

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-print(123)
-print(11)
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "Hello world!"
+    #return render_template("index.html",name="World")
+
+#@app.route("/<name>")
+#def greeting(name):
+#    return render_template("index.html",name=name)
+
+
+
+if __name__=="__main__":
+    app.run()

56dd1a2db5e048bec00c5ee5f162c9fb40ec5400
drPilman <doktorpilman@gmail.com>
2021-09-19 14:07:56 +0300

    form + jinja

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
-    return "Hello world!"
-    #return render_template("index.html",name="World")
-
-#@app.route("/<name>")
-#def greeting(name):
-#    return render_template("index.html",name=name)
+    return render_template("index.html", name="World")
 
 
+def magic(s):
+    return ''.join([c.upper() if i & 1 == 1 else c.lower() for i, c in enumerate(s)])
 
-if __name__=="__main__":
-    app.run()
+
+@app.route("/dict", methods=["post"])
+def process():
+    user = None
+    if request.method == 'POST':
+        print("1111111111111111111")
+        user = {'fname': magic(request.form.get('userfname')),
+                'lname': magic(request.form.get('userlname'))}
+    return render_template('index.html', user=user)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)

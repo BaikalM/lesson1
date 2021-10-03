dde8ad087e11fd942a61449dcdef8840429dd6b9
BaikalM <doktorpilman@gmail.com>
2021-10-03 12:14:19 +0300

    remove route /dict

--- main.py
+++ main.py
@@ -1,25 +1,14 @@
 from flask import Flask, render_template, request
+from translate import get
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html", name="World")
-
-
-def magic(s):
-    return ''.join([c.upper() if i & 1 == 1 else c.lower() for i, c in enumerate(s)])
-
-
-@app.route("/dict", methods=["post"])
+@app.route("/", methods=["post", "get"])
 def process():
-    user = None
-    if request.method == 'POST':
-        print("1111111111111111111")
-        user = {'fname': magic(request.form.get('userfname')),
-                'lname': magic(request.form.get('userlname'))}
-    return render_template('index.html', user=user)
+    return render_template('index.html',
+                           data=get(request.form.get('textarea', ''),
+                                    request.form.get('action', '')))
 
 
 if __name__ == "__main__":
